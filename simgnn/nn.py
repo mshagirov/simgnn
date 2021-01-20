fb7b0ef462461b48c466bdf3c5150525f682b81d
mshagirov <muratshagirov@gmail.com>
2021-01-20 09:22:09 +0000

    added MLP torch Module in nn

---
+++ simgnn/nn.py
@@ -0,0 +1,36 @@
+import torch
+from torch.nn import Sequential, Linear, ReLU, Dropout
+
+class mlp(torch.nn.Module):
+    '''
+    MLP consisting of multiple linear layers w/ activation func-s (`Fn`). Last layer is always linear layer w/o activation.
+    '''
+    
+    def __init__(self, in_features, out_features, hidden_dims=[], dropout_p=0, Fn=ReLU, Fn_kwargs={}):
+        '''
+        - in_features : input dim-s.
+        - out_features: output dim-s. 
+        - hidden_dims : a list of hidden dim-s (number of hidden layer neurons) {default : [] an empty list, i.e. no hidden layers}
+        - dropout_p   : dropout prob-y for hidden layer(s) {default :  0}.
+        - Fn : activation function for hidden layers { default: ReLU }
+        - Fn_kwargs : keyword arg-s for `Fn` {default : an empty dict.}
+        
+        NOTE: The last layer is always linear, i.e. it has no dropout and activation.
+        '''
+        super(mlp, self).__init__()
+        
+        layers_in = [in_features] + hidden_dims # in_features for all layers
+        
+        layers = []
+        for l, hn in enumerate(hidden_dims):
+            layers.append( Linear( layers_in[l], hn)) #append first and hidden layers
+            layers.append( Fn(**Fn_kwargs))
+            if dropout_p:
+                layers.append( Dropout(p=dropout_p))
+        layers.append( Linear( layers_in[-1], out_features))
+        
+        self.layers = Sequential(*layers)
+    
+    def forward(self, x):
+        return self.layers(x)
+
