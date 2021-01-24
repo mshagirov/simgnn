5020bbce26479b62b03f03f3cf52e13da5089f6b
mshagirov <muratshagirov@gmail.com>
2021-01-24 18:19:48 +0000

    Added Message, AggregateUpdate, and a plain Aggregate classes

--- simgnn/nn.py
+++ simgnn/nn.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import Sequential, Linear, ReLU, Dropout
+from torch_scatter import scatter
 
 class mlp(torch.nn.Module):
     '''
@@ -35,22 +36,76 @@ class mlp(torch.nn.Module):
         return self.layers(x)
 
 
-class EdgeModel(torch.nn.Module):
+class Message(torch.nn.Module):
+    '''Updates a graph's edge features by computing messages `mlp([x_src, x_tgt, edge_attr])--> new edge_attr`.'''
+    
     def __init__(self, in_features, out_features, **mlp_kwargs):
         '''
-        Arg-s:
+        MLP Arg-s:
         - in_features : input dim-s == `#src_features` + `#tgt_features` + `#edge_features`.
         - out_features: output dim-s, e.g. `#edge_features`. 
         
         Optional kwargs for `mlp`: hidden_dims =[], dropout_p = 0, Fn = ReLU, Fn_kwargs = {}.
         '''
-        super(EdgeModel, self).__init__()
-        self.edge_mlp = mlp(in_features, out_features, **mlp_kwargs)
+        super(Message, self).__init__()
+        self.mlp = mlp(in_features, out_features, **mlp_kwargs)
 
     def forward(self, src, tgt, edge_attr):
         '''
         - src, tgt : source and target features w/ shapes (#edges, #src_features) and (#edges, #tgt_features)
         - edge_attr : edge features w/ shape (#edges, #edge_features)
         '''
-        return self.edge_mlp( torch.cat( [src, tgt, edge_attr], 1) )
+        return self.mlp( torch.cat( [src, tgt, edge_attr], 1) )
+
+
+class AggregateUpdate(torch.nn.Module):
+    '''Aggregates messages (`edge_attr`) from neighbouring nodes and updates node attributes.'''
+    
+    def __init__(self, in_features, out_features, aggr='mean', **mlp_kwargs):
+        '''
+        Arg-s:
+        - in_features : input dim-s == `#node_features + #edge_features`. (MLP for updating x:node_features)
+        - out_features: output dim-s, updated node fetaures, `#new_node_features`. (MLP for updating x:node_features)
+        - aggr : aggregation scheme, one of `['sum', 'mul', 'mean', 'min', 'max']` {default: 'mean'}.
+        
+        Optional kwargs for `mlp`: hidden_dims =[], dropout_p = 0, Fn = ReLU, Fn_kwargs = {}.
+        '''
+        super(AggregateUpdate, self).__init__()
+        assert aggr in ['sum', 'mul', 'mean', 'min', 'max']
+        self.aggr = aggr
+        self.mlp = mlp(in_features, out_features, **mlp_kwargs)
+
+    def forward(self, x, edge_index, edge_attr):
+        '''
+        - x : node features w/ shape (#nodes, #node_features)
+        - edge_index : edge index (pairs of "src" and "tgt" indices) w/ shape (2, #edges)
+        - edge_attr : edge features w/ shape (#edges, #edge_features)
+        '''
+        row, col = edge_index # (src, tgt) indices
+        out = scatter(edge_attr, col, dim=0, dim_size=x.size(0), reduce=self.aggr) # aggregate mssgs at "targets"
+        out = torch.cat([x, out], dim=1) # concat w/ tgt node features
+        out = self.mlp( out ) # update node features
+        return out
+
+
+class Aggregate(torch.nn.Module):
+    '''Aggregates messages (`edge_attr`) from neighbouring nodes.'''
+    
+    def __init__(self, aggr='mean'):
+        '''
+        Arg-s:
+        - aggr : aggregation scheme, one of `['sum', 'mul', 'mean', 'min', 'max']` {default: 'mean'}.
+        '''
+        super(Aggregate, self).__init__()
+        assert aggr in ['sum', 'mul', 'mean', 'min', 'max']
+        self.aggr = aggr
+
+    def forward(self, dim_size, edge_index, edge_attr):
+        '''
+        - dim_size : number of output nodes :int
+        - edge_index : edge index (pairs of "src" and "tgt" indices) w/ shape (2, #edges)
+        - edge_attr : edge features w/ shape (#edges, #edge_features)
+        '''
+        out = scatter(edge_attr, edge_index[1], dim=0, dim_size=dim_size, reduce=self.aggr) # aggregate mssgs at "targets"
+        return out
 
