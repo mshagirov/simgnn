b5b380da5dd8f0d578bc6207f7ce78d69d6d72ba
mshagirov <muratshagirov@gmail.com>
2021-05-12 03:48:15 +0800

    added model training function need to review and try training models

---
+++ simgnn/train.py
@@ -0,0 +1,129 @@
+import time
+import copy
+import torch
+
+# Default loss functions
+mse_loss = torch.nn.MSELoss(reduction='mean')
+l1_loss = torch.nn.L1Loss(reduction='mean')
+
+def train_model(model,
+                data_loaders,
+                optimizer,
+                num_epochs = 5,
+                scheduler = None,
+                device = torch.device('cpu'),
+                model_states = ['train', 'val', 'hara'],
+                loss_func = l1_loss,
+                return_best = False):
+    '''
+    Arg-s:
+    - model : torch.nn.Module, should accept pt-geometric graph and return tuple
+    (node_output, edge_output, cell_output), i.e. `X_vel, E_tens, C_pres = model(data)`
+    - data_loaders : dict of datasets with `model_states` as keys.
+    - optimizer, num_epochs, scheduler: optimizer (e.g. SGD), number of total epochs, and
+                                        scheduler.
+    - device : torch.device, should be same as the model's device.
+    - model_states : list of dataset names, first two elem-s are always ['train', 'val']
+    - loss_func : loss function for training w/ reduction='mean'. Same loss function is
+                  used for all var types (i.e. node, edge, cell).
+                  Total batch losses are weighted by #nodes in each batch.
+    '''
+    assert num_epochs>0
+    print(f'Training param-s:', end=' ')
+
+    # init loss tracking
+    loss_categories = ['total','vel', 'tens', 'pres']
+
+    train_log = {state+'_loss_'+k: [] for state in model_states for k in loss_categories}
+    running_losses = {k:0.0 for k in train_log}
+    n_samples = {k:0.0 for k in train_log}
+
+    # logs
+    train_log['total_epochs'] = num_epochs
+    train_log['loss_func'] = loss_func.__repr__()
+    train_log['optimizer'] = optimizer.__repr__()
+    train_log['scheduler'] = 'none' if scheduler is None else scheduler.__class__.__name__
+    train_log['scheduler_params'] = 'none' if scheduler is None else scheduler.state_dict().__repr__()
+    train_log['return_best'] = return_best
+    train_log['model'] = model.__repr__()
+
+    print(f"#epochs={num_epochs}, metric={train_log['loss_func']}, "+
+          f"optim={train_log['optimizer']}, sch-r={train_log['scheduler']}, "+
+          f"return_best={train_log['return_best']}",end="\n---\n")
+
+    time_start = time.time()
+    best_wts = None
+    best_loss = 0.0
+
+    loss_func = loss_funcs[loss_metric]
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}', end=' : ')
+        for state in model_states:
+            if state == 'train':
+                model.train() # training mode
+            else:
+                model.eval() # evaluation mode
+
+            for k in loss_categories:
+                running_losses[f'{state}_loss_{k}']=0.0 # reset loss accumulators
+                n_samples[f'{state}_loss_{k}']=0
+
+            for data in data_loaders[state]:
+                data = data.to(device)
+                optimizer.zero_grad() # zero grad accumulator
+
+                with torch.set_grad_enabled(state=='train' and metric==loss_metric):
+                    X_vel, E_tens, C_pres = model(data) # outputs:(batch, #dims)
+                    vel_loss    = loss_func(X_vel, data.y)
+                    tens_loss   = loss_func(E_tens, data.edge_tensions)
+                    pres_loss   = loss_func(C_pres, data.cell_pressures)
+
+                    loss = vel_loss + tens_loss + pres_loss # total loss
+
+                    if state=='train':
+                        loss.backward()
+                        optimizer.step()
+                # accumulate losses
+                running_losses[f'{state}_loss_total'] += loss.item()*data.x.size(0)
+                n_samples[f'{state}_loss_total'] += data.x.size(0)
+
+                running_losses[f'{state}_loss_vel'] += vel_loss.item()*data.x.size(0)
+                n_samples[f'{state}_loss_vel'] += data.x.size(0)
+
+                running_losses[f'{state}_loss_tens'] += tens_loss.item()*data.edge_tensions.size(0)
+                n_samples[f'{state}_loss_tens'] += data.edge_tensions.size(0)
+
+                running_losses[f'{state}_loss_pres'] += pres_loss.item()*data.cell_pressures.size(0)
+                n_samples[f'{state}_loss_pres'] += data.cell_pressures.size(0)
+
+            for k in loss_categories:
+                # log losses for dataset==state
+                train_log[f'{state}_loss_{k}'].append( running_losses[f'{state}_loss_{k}']/n_samples[f'{state}_loss_{k}'])
+                print(f'{state}_loss_{k}={train_log[f'{state}_loss_{k}'][-1]:.4f}',end='; ')
+            print('| ',end='')
+
+            if state == 'val' and epoch==0 :
+                best_loss = train_log['val_loss_total'][-1]
+                if return_best:
+                    best_wts = copy.deepcopy(model.state_dict()) # best weights
+
+            if state == 'val' and train_log['val_loss_total'][-1] < best_loss:
+                best_loss = train_log['val_loss_total'][-1]
+                if return_best:
+                    best_wts = copy.deepcopy(model.state_dict()) # best weights
+
+        print(f'{time.time() - time_start:.0f}s')
+
+        # apply LR schedule
+        if scheduler!=None:
+            scheduler.step()
+
+    time_elapsed = time.time() - time_start
+    print(f'Training complete in {time_elapsed//60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f} (return best:{return_best})')
+
+    if return_best:
+        model.load_state_dict(best_wts) # load best model weights
+
+    return model, train_log
