3328cdbfcc26a26f625c97884dcfc353be88c963
mshagirov <muratshagirov@gmail.com>
2021-08-11 04:10:58 +0800

    added HaraAblation dataset to datasets.py

--- simgnn/datasets.py
+++ simgnn/datasets.py
@@ -105,6 +105,12 @@ class VertexDynamics(Dataset):
         super(VertexDynamics, self).__init__(root, transform, pre_transform)
         # super's __init__ runs process() [and download() if defined].
 
+    def apply_smoothing(self, v_pos):
+        '''
+        Apply smoothing along 0th axis of `v_pos` w/given `smoothing` and `sma_lag_time` parameters.
+        '''
+        return simple_moving_average(v_pos, self.sma_lag_time)
+
     @property
     def raw_file_names(self):
         raw_dirs = [folder_i for folder_i in listdir(self.raw_dir_path)
@@ -207,7 +213,7 @@ class VertexDynamics(Dataset):
         vx_pos = load_array(pos_path) #TxNx2
 
         if self.smoothing:
-            vx_pos = simple_moving_average(vx_pos, self.sma_lag_time)
+            vx_pos = self.apply_smoothing(vx_pos)
 
         vx_vel = np.diff(vx_pos,n=1,axis=0) # velocity(1st diff approx):(T-1)xNx2
 
@@ -388,3 +394,134 @@ class HaraMovies(VertexDynamics):
                 if self.pre_transform is not None:
                     data = self.pre_transform(data)
                 torch.save(data, path.join(self.processed_dir, 'data_{}_{}.pt'.format(mov_name, t)))
+
+
+class HaraAblation(VertexDynamics):
+    '''
+    For working with Y. Hara et al. amnioserosa ablation data. Hara ablation dataset *does not* have cell pressures, edge tensions,
+    and T+0 velocities. Instead of edge tensions `HaraAblation` contains edge recoil results from laser ablation experiments.
+
+    Overwrites following methods in `VertexDynamics`:
+        - `processed_file_names`
+        - `process`
+        - `pos2nodeXY`
+    '''
+
+    def __init__(self, root, window_size=5, transform=None, pre_transform=None, smoothing=False, sma_lag_time=None):
+        '''
+        Assumes `root` dir contains folder named `raw` with following files that contain results
+        for tracing vertex trajectories, building graphs:
+        - vtx_pos.npy : shape (Frames, Vertices, 2), positions of tri-cellular junctions.
+        - edges_index.npy : shape (2, Edges), edge indices-- each column contains indices of source and target vertices/nodes.
+        - node2cell_index.npy : shape (2, Node-to-Cell edges), first row contains indices of the nodes/vertices, and second
+                                row contains indices of the corresponding cell IDs (cell indices).
+        - edge_recoils.npy : recoil values for all edges. All values are "np.nan" except for target edge(s), shape (Edges,).
+                             E.g. `[nan,..., nan, 0.0238768, nan,...,nan]`.
+        - frames.npy: (optional) frame numbers for the first dimension of array in `vtx_pos.npy`, shape (Frames,).
+
+        Arg-s:
+        - root : path to a root directory that contains folder with raw dataset(s) in a folder named "raw".
+        Raw datasets should be placed into separate folders each containing outputs from a single ablation sample.
+        E.g. root contains ["raw", "processed", ...], and in folder "raw/" we should have ["sample_i", "sample_j", ...]
+        - window_size : number of past velocities to be used as node features ordered from `T-window_size` to `T-1`
+                       `[x(T-window_size+1)-x(T-window_size),..., x(T-1)-x(T-2), x(T+0)-x(T-1)]`, where `x(T)` is
+                       node position at time `T`. For more details see method `VertexDynamics.pos2nodeXY()`.
+        - transform :  transform(s) for graph datasets (e.g. from torch_geometric.transforms ), used in parent class' loading method.
+        - pre_transform : transform(s) for data pre-processing (resulting graphs are saved in "preprocessed" folder)
+        and used as this dataset's sample graphs.
+
+        Notes:
+        - Velocities are approximated as 1st order differences of positions `x` (vtx_pos.npy) in subsequent frames:
+          `velocity(T+0) = x(T+1) - x(T+0)` (*only* for features, as T+0 is absent in ablation data).
+        - Use `pre_transform` for normalising and pre-processing dataset(s).
+        - To be a valid sample in `HaraAblation` dataset, an ablation movie should have at least `window_size+1` frames,
+          otherwise an example is ignored. `HaraAblation` uses the last `window_size+1` number of frames to produce
+          `window_size` number of past velocities (`x`) and T+0 positions (`pos`).
+        '''
+        super(HaraAblation, self).__init__(root, window_size=window_size, transform=transform, pre_transform=pre_transform,
+                                         smoothing=smoothing, sma_lag_time=sma_lag_time)
+
+    @property
+    def processed_file_names(self):
+        '''
+        Return list of pytorch-geometric data files in `root/processed` folder (`self.processed_dir`).
+        '''
+        # "last_idx" : last index of window in "vertex velocity" (for features)
+        # last_idx=T-(2+window_size) --> num of processed frames: num_of_frames=last_idx+1
+        # movie should have at least `window_size`+1 frames
+        valid_data_names = [ path.basename(raw_path) for raw_path in self.raw_paths
+                            if load_array(path.join(raw_path,'vtx_pos.npy')).shape[0]>self.window_size]
+        file_names = ['{}.pt'.format(n) for n in valid_data_names]
+        return file_names
+
+    def process(self):
+        '''
+        Assumptions:
+        - the parent class init runs _process() and initialises all the required dir-s.
+        - cell graph topology and number of nodes is constant w.r.t. to frames.
+        '''
+        for raw_path in self.raw_paths:
+            # Load node positions from raw_path and convert to (windowed) node attrib-s and targets.
+            node_pos, X_node = self.pos2nodeXY(pos_path = path.join(raw_path,'vtx_pos.npy') )
+
+            # edge indices
+            edge_index = torch.from_numpy( load_array( path.join( raw_path, 'edges_index.npy'))
+                                         ).type(torch.long).contiguous()
+
+            # edge recoils
+            edge_recoils = torch.from_numpy( load_array( path.join( raw_path, 'edge_recoils.npy'))
+                                          ).type(dtype)
+
+            # cell-to-node and node-to-cell "edge indices"
+            node2cell_index = torch.from_numpy( load_array( path.join( raw_path, 'node2cell_index.npy'))
+                                              ).type(torch.long).contiguous() # node_id-cell_id pairs
+            cell2node_index = node2cell_index[[1,0]].contiguous() # cell_id-node_id pairs
+
+            N_nodes = node_pos.size(1) # number of vertices/nodes (assume constant w.r.t. time)
+            N_cells = cell2node_index[0].max().item() + 1 # num_of_cells (assume constant w.r.t. time)
+
+            data = CellData(num_nodes = N_nodes,
+                            num_cells = N_cells,
+                            edge_index = edge_index,
+                            node2cell_index = node2cell_index, cell2node_index = cell2node_index,
+                            pos = node_pos, x = X_node, y = None,
+                            cell_pressures = None,
+                            edge_tensions = None, edge_recoils = edge_recoils
+                           )
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+            torch.save(data, path.join(self.processed_dir, '{}.pt'.format( path.basename(raw_path) ) ))
+
+    def pos2nodeXY(self, pos_path=None):
+        '''
+        Load 'simul_vtxpos.npy' and pre-process it into windowed data of velocities (1st differences) using
+        `self.window_size` number of past velocities (frames) as node features.
+
+        - pos_path: path to vertex positions file, e.g. 'vtx_pos.npy', containing an array with shape `(frames)xNx2`.
+
+        Returns: {dtype : `torch.float32`}
+        - node_pos: node positions : shape Nx2
+        - X_node: node velocities ordered from `T-window_size` to `T-1` : shape Nx(window_size)x2
+
+        Where the `num_of_frames=last_idx+1`, and `last_idx=frames-(2+window_size)` is the last index of window in vx_vel
+        (full array of 1st differences of the node positions with "frames-1" number of frames, i.e. vx_pos.shape[0]==frames).
+
+        Note that `Y_node` the `T+0` vertex velocities are not available.
+        '''
+        # vertex trajectories:(Frames,Vertices,Dims)=TxNx2
+        vx_pos = load_array(pos_path) #TxNx2
+
+        if self.smoothing:
+            vx_pos = self.apply_smoothing(vx_pos)
+
+        vx_vel = np.diff(vx_pos,n=1,axis=0) # velocity(1st diff approx):(T-1)xNx2
+
+        # T+0 vertex positions
+        node_pos = torch.from_numpy( vx_pos[-1]).type(dtype) # Nx2
+
+        # last (T-window_size) to (T-1) velocities : Nx(window_size)x2
+        X_node = torch.from_numpy( vx_vel[vx_vel.shape[0]-self.window_size:vx_vel.shape[0]].transpose((1,0,2))
+                                 ).type(dtype)
+        return node_pos, X_node
